c2fee45e9726b9028031d33832fe10978c170f87
Shahar Melamed <shaharonnn@gmail.com>
2020-12-14 23:43:17 +0200

    Initial Commit

---
+++ Bibi/BibiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibiApp.swift
+//  Bibi
+//
+//  Created by Shahar Melamed on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct BibiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
